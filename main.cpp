bb856416f75f648906b08967d9d198a72eb1634a
Barsbek Azamatov <bars.moderside@gmail.com>
2026-05-21 16:11:22 -0700

    update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <deque>
+#include <queue>
 
 using namespace std;
 
@@ -19,6 +21,9 @@ struct Node {
 void addRandomCustomer(Node*& front, Node*& back,
                        string names[], string drinks[]);
 
+void addRandomMuffinCustomer(deque<Node>& muffinLine,
+                             string names[], string muffins[]);
+
 int main() {
     srand(time(nullptr));
     string names[SIZE_ARR] {
@@ -31,11 +36,21 @@ int main() {
         "Cold Brew", "Iced Coffee", "Macchiato", "Chai Latte", "Hot Chocolate"
     };
 
+    string muffins[SIZE_ARR] {
+        "Blueberry Muffin", "Chocolate Chip Muffin", "Banana Nut Muffin",
+        "Lemon Poppy Muffin", "Bran Muffin", "Pumpkin Muffin",
+        "Apple Cinnamon Muffin", "Double Chocolate Muffin",
+        "Raspberry Muffin", "Corn Muffin"
+    };
+
     Node* front{};
     Node* back{};
 
+    deque<Node> muffinLine;
+
     for (int i{0}; i < START_CUSTOMERS; ++i) {
         addRandomCustomer(front, back, names, drinks);
+        addRandomMuffinCustomer(muffinLine, names, muffins);
     }
 
     for (int numRound{1}; numRound <= MAX_ROUNDS; ++numRound) {
@@ -53,11 +68,20 @@ int main() {
             cout << "No customer served.\n";
         }
 
-        if (rand() % 2 == 0) {
+        if (rand() % 2 == 0) { // 50
             addRandomCustomer(front, back, names, drinks);
             cout << "New customer joined\n";
         } else {
             cout << "No new custmer joined\n";
+        }
+
+        if (!muffinLine.empty()) {
+            cout << "Muffin served: " << muffinLine.front().name << ":"
+                 << muffinLine.front().drinkOrder << '\n';
+
+            muffinLine.pop_front();
+        } else {
+            cout << ""
         }
     }
 
@@ -81,4 +105,14 @@ void addRandomCustomer(Node*& front, Node*& back,
         back->next = customer;
         back = customer;
     }
+}
+
+void addRandomMuffinCustomer(deque<Node>& muffinLine,
+                             string names[], string muffins[]) {
+    Node muffinCustomer;
+    muffinCustomer.name = names[rand() % SIZE_ARR];
+    muffinCustomer.drinkOrder = muffins[rand() % SIZE_ARR];
+    muffinCustomer.next = nullptr;
+
+    muffinLine.push_back(muffinCustomer);
 }
