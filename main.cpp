32e7a9e6e12bb298b5aa6808ba48f494b1e53f2f
Barsbek Azamatov <bars.moderside@gmail.com>
2026-05-21 14:59:47 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
+
 
 using namespace std;
 
+struct Node {
+    string name;
+    string drinkOrder;
+    Node* next;
+};
+
 int main() {
+
+
     return 0;
 }
